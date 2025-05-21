9bdd8c9c3b2f202242a69f6d2a40c3a5363ace46
Suba-Rajendran <subarajendrann@gmail.com>
2025-05-21 10:29:50 +0530

    Fix mongoose connection issue

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config(); // Optional if you're using a .env file
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connection
-MONGODB_URI=mongodb+srv ;//subasuba52621:<your_password>@cluster0.v2tgzo5.mongodb.net/paavaivastra?retryWrites=true&w=majority&appName=Cluster0
-  .then(() => console.log('MongoDB Connected'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+async function connectToMongoDB() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('MongoDB Connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+  }
+}
 
-// Sample route
+connectToMongoDB();
+
 app.get('/', (req, res) => {
-  res.send('Welcome to Paavai Vastra Backend!');
+  res.send('Server is working!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
